c70a4e636633c369f69e05529bf291bb5d40e690
torbatti <torbattii@gmail.com>
2024-12-15 01:02:11 +0330

    initial

---
+++ src/io.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const os = std.os;
+
+const IO_Linux = @import("io/linux.zig").IO;
+// const IO_Darwin = @import("io/darwin.zig").IO;
+// const IO_Windows = @import("io/windows.zig").IO;
+
+pub const IO = switch (builtin.target.os.tag) {
+    .linux => IO_Linux,
+    // .windows => IO_Windows,
+    // .macos, .tvos, .watchos, .ios => IO_Darwin,
+    else => @compileError("IO is not supported for platform"),
+};
