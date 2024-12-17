f26e83295f0c2448e3ee01736b7527984fffd9da
torbatti <torbattii@gmail.com>
2024-12-18 00:54:23 +0330

    init

---
+++ src/protocol.zig
@@ -0,0 +1,5 @@
+pub const varint = @import("protocol-core/varint.zig");
+pub const nbt = @import("protocol-core/nbt.zig");
+pub const serde = @import("protocol-core/serde.zig");
+
+pub const v765 = @import("protocol-core/v765.zig");
