a0facf0637d376f797b3cff631b66ae59eef2838
torbatti <torbattii@gmail.com>
2024-12-26 18:19:33 +0330

    update: test if seeb module works inside main

--- build.zig
+++ build.zig
@@ -23,92 +23,204 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "seeb",
-        .root_source_file = b.path("src/seeb.zig"),
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-    lib.addCSourceFile(.{
-        .file = b.path("include/sqlite3.c"),
-        .flags = &[_][]const u8{
-            // default c flags:
-            "-Wall",
-            "-Wextra",
-            "-std=c2x",
-            "-pedantic",
+    // Top-level steps you can invoke on the command line.
+    const build_steps = .{
+        .check = b.step("check", "Check if Seeb compiles"),
+        .static = b.step("static", "Run Seeb"),
+        .dynamic = b.step("dynamic", "Run Seeb"),
+        .run = b.step("run", "Run Seeb"),
+        .fuzz = b.step("fuzz", "Run fuzzers"),
+        .@"test" = b.step("test", "Run all tests"),
+    };
 
-            // Recommended Compile-time Options:
-            // https://www.sqlite.org/compile.html
-            "-DSQLITE_DQS=0",
-            "-DSQLITE_DEFAULT_WAL_SYNCHRONOUS=1",
-            "-DSQLITE_USE_ALLOCA=1",
-            "-DSQLITE_THREADSAFE=1",
-            "-DSQLITE_TEMP_STORE=3",
-            "-DSQLITE_ENABLE_API_ARMOR=1",
-            "-DSQLITE_ENABLE_UNLOCK_NOTIFY",
-            "-DSQLITE_ENABLE_UPDATE_DELETE_LIMIT=1",
-            "-DSQLITE_DEFAULT_FILE_PERMISSIONS=0600",
-            "-DSQLITE_OMIT_DECLTYPE=1",
-            "-DSQLITE_OMIT_DEPRECATED=1",
-            "-DSQLITE_OMIT_LOAD_EXTENSION=1",
-            "-DSQLITE_OMIT_PROGRESS_CALLBACK=1",
-            "-DSQLITE_OMIT_SHARED_CACHE",
-            "-DSQLITE_OMIT_TRACE=1",
-            "-DSQLITE_OMIT_UTF16=1",
-            "-DHAVE_USLEEP=0",
-        },
-    });
-    lib.installHeader(b.path("include/sqlite3.h"), "sqlite3.h");
-    b.installArtifact(lib);
+    // const lib_module = build_lib_module(b, .{
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const exe = b.addExecutable(.{
-        .name = "seeb",
-        .root_source_file = b.path("src/seeb/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe);
+    // zig build check
+    // build_check(b, build_steps.check, .{
+    //     .lib_module = lib_module,
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const run_cmd = b.addRunArtifact(exe);
+    // zig build, zig build run
+    // build_seeb(b, build_steps.run, .{
+    //     .lib_module = lib_module,
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    run_cmd.step.dependOn(b.getInstallStep());
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "seeb",
+    //     .root_source_file = b.path("src/seeb.zig"),
+    //     .link_libc = true,
+    // });
+    // lib.addCSourceFile(.{
+    //     .file = b.path("include/sqlite3.c"),
+    //     .flags = &[_][]const u8{
+    //         // default c flags:
+    //         "-Wall",
+    //         "-Wextra",
+    //         // "-std=c2x",
+    //         "-std=c99",
+    //         "-pedantic",
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    //         // Recommended Compile-time Options:
+    //         // https://www.sqlite.org/compile.html
+    //         "-DSQLITE_DQS=0",
+    //         "-DSQLITE_DEFAULT_WAL_SYNCHRONOUS=1",
+    //         "-DSQLITE_USE_ALLOCA=1",
+    //         "-DSQLITE_THREADSAFE=1",
+    //         "-DSQLITE_TEMP_STORE=3",
+    //         "-DSQLITE_ENABLE_API_ARMOR=1",
+    //         "-DSQLITE_ENABLE_UNLOCK_NOTIFY",
+    //         "-DSQLITE_ENABLE_UPDATE_DELETE_LIMIT=1",
+    //         "-DSQLITE_DEFAULT_FILE_PERMISSIONS=0600",
+    //         "-DSQLITE_OMIT_DECLTYPE=1",
+    //         "-DSQLITE_OMIT_DEPRECATED=1",
+    //         "-DSQLITE_OMIT_LOAD_EXTENSION=1",
+    //         "-DSQLITE_OMIT_PROGRESS_CALLBACK=1",
+    //         "-DSQLITE_OMIT_SHARED_CACHE",
+    //         "-DSQLITE_OMIT_TRACE=1",
+    //         "-DSQLITE_OMIT_UTF16=1",
+    //         "-DHAVE_USLEEP=0",
+    //     },
+    // });
+    // lib.installHeader(b.path("include/sqlite3.h"), "sqlite3.h");
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // const exe = b.addExecutable(.{
+    //     .name = "seeb",
+    //     .root_source_file = b.path("src/seeb/main.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // b.installArtifact(exe);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // const run_cmd = b.addRunArtifact(exe);
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    // run_cmd.step.dependOn(b.getInstallStep());
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // // This allows the user to pass arguments to the application in the build
+    // // command itself, like this: `zig build run -- arg1 arg2 etc`
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);fn build_lib_module(b: *std.Build, options: struct {
+    //     target: std.Build.ResolvedTarget,
+    //     optimize: std.builtin.OptimizeMode,
+    // }) *std.Build.Module {
+    //     const lib_module = b.addModule("seeb", .{
+    //         .root_source_file = b.path("src/seeb.zig"),
+    //         .target = options.target,
+    //         .optimize = options.optimize,
+    //     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    //     return lib_module;
+    // }
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    // fn build_seeb(b: *std.Build, step_build_seeb: *std.Build.Step, options: struct {
+    //     lib_module: *std.Build.Module,
+    //     target: std.Build.ResolvedTarget,
+    //     optimize: std.builtin.OptimizeMode,
+    // }) void {
+    //     const seeb = b.addStaticLibrary(.{
+    //         .name = "seeb",
+    //         .root_source_file = b.path("src/seeb/main.zig"),
+    //         .target = options.target,
+    //         .optimize = options.optimize,
+    //     });
+    //     seeb.root_module.addImport("seeb", options.lib_module);
+
+    //     const run_cmd = b.addRunArtifact(seeb);
+    //     run_cmd.step.dependOn(b.getInstallStep());
+    //     if (b.args) |args| {
+    //         run_cmd.addArgs(args);
+    //     }
+    //     step_build_seeb.dependOn(&run_cmd.step);
+    // }
+    // }
+
+    // // This creates a build step. It will be visible in the `zig build --help` menu,
+    // // and can be selected like this: `zig build run`
+    // // This will evaluate the `run` step rather than the default, which is "install".
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
+
+    // // Creates a step for unit testing. This only builds the test executable
+    // // but does not run it.
+    // const lib_unit_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    // const exe_unit_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/main.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    // // Similar to creating the run step earlier, this exposes a `test` step to
+    // // the `zig build --help` menu, providing a way for the user to request
+    // // running the unit tests.
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&run_lib_unit_tests.step);
+    // test_step.dependOn(&run_exe_unit_tests.step);
 }
+
+// fn build_lib_module(b: *std.Build, options: struct {
+//     target: std.Build.ResolvedTarget,
+//     optimize: std.builtin.OptimizeMode,
+// }) *std.Build.Module {
+//     const lib_module = b.addModule("seeb", .{
+//         .root_source_file = b.path("src/seeb.zig"),
+//         .target = options.target,
+//         .optimize = options.optimize,
+//     });
+
+//     return lib_module;
+// }
+
+// fn build_seeb(b: *std.Build, step_build_seeb: *std.Build.Step, options: struct {
+//     lib_module: *std.Build.Module,
+//     target: std.Build.ResolvedTarget,
+//     optimize: std.builtin.OptimizeMode,
+// }) void {
+//     const seeb = b.addStaticLibrary(.{
+//         .name = "seeb",
+//         .root_source_file = b.path("src/seeb/main.zig"),
+//         .target = options.target,
+//         .optimize = options.optimize,
+//     });
+//     seeb.root_module.addImport("seeb", options.lib_module);
+
+//     const run_cmd = b.addRunArtifact(seeb);
+//     run_cmd.step.dependOn(b.getInstallStep());
+//     if (b.args) |args| {
+//         run_cmd.addArgs(args);
+//     }
+//     step_build_seeb.dependOn(&run_cmd.step);
+// }
+
+// fn build_check(
+//     b: *std.Build,
+//     step_check: *std.Build.Step,
+//     options: struct {
+//         lib_module: *std.Build.Module,
+//         target: std.Build.ResolvedTarget,
+//         optimize: std.builtin.OptimizeMode,
+//     },
+// ) void {
+//     const seeb = b.addExecutable(.{
+//         .name = "seeb",
+//         .root_source_file = b.path("src/seeb/main.zig"),
+//         .target = options.target,
+//         .optimize = options.optimize,
+//     });
+//     seeb.root_module.addImport("seeb", options.lib_module);
+//     step_check.dependOn(&seeb.step);
+// }
--- src/seeb/main.zig
+++ src/seeb/main.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
+const seeb = @import("seeb");
 const assert = std.debug.assert;
 
 const cli = @import("cli.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    seeb.test_print();
+}
