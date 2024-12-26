4e65e9dc40278a0065c9436ec44101453e139e0e
torbatti <torbattii@gmail.com>
2024-12-26 18:19:06 +0330

    add more details

--- include/download.sh
+++ include/download.sh
@@ -1,6 +1,12 @@
-# sqlite version: 3.47.2
+#!/usr/bin/env sh
+
+SQLITE_VERSION="3.47.2"
+
+# Download Sqlite c amalgamation
 wget -O sqlite.zip \
     https://www.sqlite.org/2024/sqlite-amalgamation-3470200.zip
+
+# Extract 
 unzip sqlite.zip
 mv -f sqlite-amalgamation*/ sqlite-amalgamation/
 mv -f sqlite-amalgamation/sqlite3.c sqlite-amalgamation/sqlite3.h .
