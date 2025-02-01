f8700929ae7ee2359f12ac1402c9e5a04ff718ca
torbatti <torbattii@gmail.com>
2025-02-01 23:33:51 +0330

    update: add fromInt and toString methods for tag enum

--- src/nbt.zig
+++ src/nbt.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const Tag = enum(u8) {
     end = 0,
     byte = 1,
@@ -12,4 +14,32 @@ pub const Tag = enum(u8) {
     compound = 10,
     int_array = 11,
     long_array = 12,
+
+    pub const E = error{InvalidTag};
+
+    pub fn fromInt(val: anytype) E!Tag {
+        // const val_convert = @as(u8, @intCast(val)); // make sure val is u8 type
+        if (val >= 0 and val <= 12)
+            return @enumFromInt(val)
+        else
+            return error.InvalidTag;
+    }
+
+    pub fn toString(tag: Tag) [:0]const u8 {
+        return switch (tag) {
+            .end => "TAG_End",
+            .byte => "TAG_Byte",
+            .short => "TAG_Short",
+            .int => "TAG_Int",
+            .long => "TAG_Long",
+            .float => "TAG_Float",
+            .double => "TAG_Double",
+            .byte_array => "TAG_ByteArray",
+            .string => "TAG_String",
+            .list => "TAG_List",
+            .compound => "TAG_Compound",
+            .int_array => "TAG_IntArray",
+            .long_array => "TAG_LongArray",
+        };
+    }
 };
